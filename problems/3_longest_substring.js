29890e1d0639b1e70a29467671b25986606ceb5b
Adam Tait <bin@adamta.it>
2021-06-13 23:15:43 -0700

    problems/3: all tests passing

--- problems/3_longest_substring.js
+++ problems/3_longest_substring.js
@@ -1,23 +1,34 @@
-const { areEqual, newTestRunner } = require('../test');
-
 /**
  * @param {string} s
  * @return {number}
  */
 
-const splitRepeated = (subs, c) => {
-    const ls = subs.pop();
-    if (ls.indexOf(c) >= 0)
-        return subs.concat([ls, c]);
-    return subs.concat([ ls + c ]);
+const consume = (a) => {
+    var na = [];
+    for (var i in a) {
+        const j = Number(i);
+        const c = a[j];
+        if ( na.indexOf(c) >= 0 ) return na;
+        na.push(c); 
+    }
+    return na;
 };
 
 const lengthOfLongestSubstring = (s) => {
-    const ss = s.split('');
-    const sss = ss.reduce(splitRepeated, [''])
-          .concat(ss.reverse().reduce(splitRepeated, ['']));
 
-    return sss.reduce(
+    const ss = s.split('');
+    var rs = [];
+    
+    for (var i in ss) {
+        const j = Number(i);
+        rs.push( consume(ss.slice(j)) );
+    }
+
+    for (var i = ss.length - 1; i >= 0; i--) {
+        rs.push( consume(ss.slice(0,i).reverse()).reverse() );
+    }
+
+    return rs.reduce(
         (longest, sub) => sub.length > longest.length ? sub : longest, ''
     ).length;
 };
@@ -33,7 +44,8 @@ const inputExpectedPairs = [
     ['abcabcbb', 3],
     ['bbbbb', 1],
     ['pwwkew', 3],
-    ['dvdf', 3]
+    ['dvdf', 3],
+    ['asjrgapa', 6]
 ];
 
 module.exports = {
