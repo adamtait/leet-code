8e760ed6d3b14d1e131697d492863b10132d0e9f
Adam Tait <bin@adamta.it>
2021-08-24 20:21:39 -0700

    leet/371: add 1st attempt

--- app.js
+++ app.js
@@ -1,30 +1,32 @@
 const { test } = require('./test');
-const p1 = require('./problems/leet/1_two_sum');
-//const p2 = require('./problems/leet/2_add_two_numbers');
-const p3 = require('./problems/leet/3_longest_substring');
-const p5 = require('./problems/leet/5_longest_palindromic_string');
-const p6 = require('./problems/leet/6_zigzag_conversion');
-const p7 = require('./problems/leet/7_reverse_integer');
-const p8 = require('./problems/leet/8_string_to_integer');
-const p9 = require('./problems/leet/9_palindrome_number');
-const p10 = require('./problems/leet/10_regular_expression_matching');
-const p11 = require('./problems/leet/11_container_with_most_water');
-const p12 = require('./problems/leet/12_integer_to_roman');
-const p15 = require('./problems/leet/15_three_sum');
 
-const pt = require('./problems/other/permissions_tree');
-const pc = require('./problems/fb/cafeteria');
-const pdp = require('./problems/fb/director_of_photography');
-const d7 = require('./problems/daily/7_decoding_combinations');
-const d8 = require('./problems/daily/8_unival_subtrees');
-const d9 = require('./problems/daily/9_sum_non_adjacent');
-const d11 = require('./problems/daily/11_autocomplete');
+const d7   = require('./problems/daily/7_decoding_combinations');
+const d8   = require('./problems/daily/8_unival_subtrees');
+const d9   = require('./problems/daily/9_sum_non_adjacent');
+const d11  = require('./problems/daily/11_autocomplete');
+
+const p1   = require('./problems/leet/1_two_sum');
+//const p2   = require('./problems/leet/2_add_two_numbers');
+const p3   = require('./problems/leet/3_longest_substring');
+const p5   = require('./problems/leet/5_longest_palindromic_string');
+const p6   = require('./problems/leet/6_zigzag_conversion');
+const p7   = require('./problems/leet/7_reverse_integer');
+const p8   = require('./problems/leet/8_string_to_integer');
+const p9   = require('./problems/leet/9_palindrome_number');
+const p10  = require('./problems/leet/10_regular_expression_matching');
+const p11  = require('./problems/leet/11_container_with_most_water');
+const p12  = require('./problems/leet/12_integer_to_roman');
+const p15  = require('./problems/leet/15_three_sum');
+const p371 = require('./problems/leet/371_sum_two_integers');
+
+const pc   = require('./problems/fb/cafeteria');
+const pdp  = require('./problems/fb/director_of_photography');
+
 
 const ps = {
     daily: [d7, d8, d9, d11],
     fb: [pc, pdp],
-    leet: [p1, p3, p5, p6, p7, p8, p9, p10, p11, p12, p15],
-    other: [pt]
+    leet: [p1, p3, p5, p6, p7, p8, p9, p10, p11, p12, p15, p371]
 };
 
 const problems = Object.values(ps).reduce((a,p) => a.concat(p), []);
---
+++ problems/leet/371_sum_two_integers.js
@@ -0,0 +1,86 @@
+/** 
+ * Given two integers a and b, return the sum of the two integers
+ * without using the operators + and -.
+ *
+ * boundary: -1000 <= a, b <= 1000
+ */
+
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+
+// 01
+// 01
+// 10
+
+// 01
+// 10
+// 11
+
+// 010
+// 010
+// 100
+
+// 011
+// 011
+// 110
+
+// 0111
+// 0111
+// 1110
+
+// 0110
+// 0111
+// 1101
+
+
+
+var kthBit = (k, num) => (num & (1 << k)) !== 0 ? 1 : 0;
+
+var getSum = function(a, b) {
+
+    var s = 0;
+    var carry = 0;
+    for (var i = 0; i < 33; i++) {
+        
+        const bits = [carry, kthBit(i, a), kthBit(i, b)];
+
+        // next carry
+        carry = 0;
+        const c = bits.filter(n => n > 0).length;
+        if ( c > 1 ) carry = 1;
+
+        // update result
+        const bit = bits.reduce((a,b) => a ^ b);
+        const mask = bit << i;
+        s |=  mask;
+    }
+    return s;
+};
+
+var toBin = (n) => (n >>> 0).toString(2);
+
+
+//
+// testing
+
+const inputExpectedPairs = [
+    [[0,0], 0],
+    [[0,1], 1],
+    [[1,1], 2],
+    [[1,2], 3],
+    [[2,3], 5],
+    [[1000,1000], 2000],
+    [[-1,1], 0],           // fails => 1
+    [[-1,-1], -2],         // fails => -1
+    [[-1000,-1000], -2000],
+    [[-1000,1000], 0]
+];
+
+module.exports = {
+    f: getSum,
+    inputExpectedPairs,
+    name: '371 - sum of two integers (binary)'
+};
