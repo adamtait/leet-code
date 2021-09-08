5acdbe09b7d146cf980b9bbe70d22e82b8632540
Adam Tait <bin@adamta.it>
2021-09-08 13:47:33 -0700

    problems/cake/merging_ranges: add solution based on splitting ranges into events. write up accepted solution.

--- problems/cake/merging_ranges.js
+++ problems/cake/merging_ranges.js
@@ -1,8 +1,10 @@
 
 /**
- *  Your company built an in-house calendar tool called HiCal. You
- *  want to add a feature to see the times in a day when everyone is
- *  available.
+ * url: https://www.interviewcake.com/question/javascript/merging-ranges
+ *
+ * Your company built an in-house calendar tool called HiCal. You
+ * want to add a feature to see the times in a day when everyone is
+ * available.
  *
  * To do this, you’ll need to know when any team is having a meeting.
  * In HiCal, a meeting is stored as objects ↴ with integer properties
@@ -47,6 +49,74 @@ var mergeRanges = (ranges) => {
     return mrs;
 };
 
+
+//
+// solution 2
+// use a stack/map to merge overlaps
+// time: O(2n) = O(n)
+// space: O(2n) = O(n)
+
+var id = 0;
+
+var rangeToEventReducer = (a, { startTime, endTime }) => {
+    const i = id++;
+    return [ ...a,
+             [startTime, id, true],
+             [endTime, id, false]
+           ];
+};
+
+var mergeRanges = (ranges) => {
+
+    // split ranges into separate start, end events. assign unique id's
+    // sort events
+    id = 0;
+    const rs = ranges
+          .reduce(rangeToEventReducer, [])
+          .sort(([at, ...a], [bt, ...b]) => at - bt);
+
+    // iterate through events
+    var r = [], currentStartTime = null, active = {};
+    for ( var i = 0; i < rs.length; i++ ) {
+        const [t, id, action] = rs[i];
+
+        // if active is empty?, then set new currentStartTime
+        if ( Object.keys(active).length === 0 ) {
+            const lastRange = r[r.length-1];
+            const lastEndTime = lastRange ? lastRange.endTime : null;
+            
+            // if next range starts at the same time as last range ended...
+            if ( lastEndTime && lastEndTime === t ) {
+                // start back at the beginning of the last range
+                currentStartTime = lastRange.startTime;
+                r.pop(); // remove the last range
+            }
+            else currentStartTime = t;
+        }
+        
+        // accumulate start events, remove end events
+        if ( action ) active[id] = t;
+        else delete active[id];
+
+        // if remove makes stack/map empty?, then complete current range
+        if ( Object.keys(active).length === 0 ) {
+            
+            r.push({ startTime: currentStartTime,
+                     endTime: t });
+            currentStartTime = null;
+        }
+    }
+    return r;
+};
+
+
+/*
+  Accepted solution
+  + sort ranges by startTime
+  + accumulate merged ranges
+  + if cur.startTime <= lastMerged.endTime, then lastMerged.endTime = max(cur.endTime, lastMerged.endTime)
+  + else add cur to lastMerged list
+*/
 
 
 //
