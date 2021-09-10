5454fbd0560e6da6f6464bd670eeab12beeef678
Adam Tait <bin@adamta.it>
2021-09-10 15:24:49 -0700

    problems/leet/741: 1st attempt (greedy)

---
+++ problems/leet/741_cherry_pickup.js
@@ -0,0 +1,154 @@
+/**
+ * url: https://leetcode.com/problems/cherry-pickup/
+ *
+ * You are given an n x n grid representing a field of cherries, each
+ * cell is one of three possible integers.
+ *
+ *   0 means the cell is empty, so you can pass through,
+ *   1 means the cell contains a cherry that you can pick up and pass through, or
+ *   -1 means the cell contains a thorn that blocks your way.
+ *
+ * Return the maximum number of cherries you can collect by following
+ * the rules below:
+ *
+ *   Starting at the position (0, 0) and reaching (n - 1, n - 1) by
+ *   moving right or down through valid path cells (cells with value 0
+ *   or 1).
+ *
+ *   After reaching (n - 1, n - 1), returning to (0, 0) by moving left
+ *   or up through valid path cells.
+ *
+ *   When passing through a path cell containing a cherry, you pick it
+ *   up, and the cell becomes an empty cell 0.
+ *
+ *   If there is no valid path between (0, 0) and (n - 1, n - 1), then
+ *   no cherries can be collected.
+ */
+
+/*
+  solution 1
+  + brute force
+  + enumerate every possible combination of paths to goal & back
+    + pick the maximum pair
+  + time:  O( 2 * n^3 )
+  + space: O( 2 * n )
+
+ solution 2
+ + Dijkstra
+ + min distance calculation needs to account for cherries...
+ + time:  O( 2 * v^2 )
+ + space: O( v )
+
+ solution 3
+ + A* search
+ + heuristic is average distance to cherries
+   + requires that we know where all the cherries are (input)
+   + may be expensive to compute
+   + may result in suboptimal path
+     + could it go in between 2 cherries?
+ + time:  O( 2 * ( v^2 * c ) ) - c is the # of cherries
+ + space: O( v )
+*/
+
+var goalPair = (g, rightDown = true) => {
+    if ( rightDown )
+        return [ g.length-1, g[0].length-1 ];
+    return [0,0];
+};
+
+//var initializeSeen = (g, rightDown  = true) {
+//    if ( rightDown )
+//        return [[true]]; // seen[0][0] === true
+//
+//    const maxi = g.length-1, maxj = g[0].length-1;
+//    var m = [];
+//    for ( var i = 0; i <= maxi; i++ ) {
+//        m[i] = [];
+//        for ( var j = 0; j <= maxj; j++ )
+//            m[i][j] = null;
+//    }
+//    m[maxi][maxj] = true;
+//    return m;
+//};
+
+
+var neighbors = (g, i, j, rightDown = true) => {
+    
+    var ns = null;
+    if ( rightDown )
+        ns = [ [i, j+1], [i+1,j] ];  // right, down
+    else ns = [ [i-1, j], [i, j-1] ]; // top, left
+
+    if ( i === 0 ) ns.filter(([a,b] => a > i));
+    if ( j === 0 ) ns.filter(([a,b] => b > j));
+
+    const maxi = g.length -1;
+    const maxj = g[0].length -1;
+    if ( i === maxi ) ns.filter(([a,b] => a < i));
+    if ( j === maxj ) ns.filter(([a,b] => b < j));
+    
+    return ns;
+};
+
+var pathValue = (g, [i,j]) => {
+    return g[i][j];
+};
+
+var nextNeighbor = (g, i, j, rightDown = true) => {
+    var ns = neighbors(g, i, j, rightDown)
+        .map(p => [p, pathValue(g,p)])
+        .filter(([p,v]) => v > -1)  // filter out walls
+        .sort(([a,av],[b,bv]) => bv - av);  // sort descending
+    return ns[0][0];
+};
+
+var maxPath = (g, rightDown = true) => {
+
+    var [goali, goalj] = goalPair(g, rightDown);
+    
+    var i = rightDown ? 0 : g.length-1;
+    var j = rightDown ? 0 : g[0].length-1;
+    var mp = [[i,j]];  // max path
+        
+    while ( i !== goali && j !== goalj ) {
+        var [ni, nj] = nextNeighbor(g, i, j, rightDown);
+        mp.push( [ni, nj] );
+        i = ni;
+        j = nj;
+    }
+    return mp;
+};
+
+var removeCherries = (g, pairs) => {
+    // update g by setting all nodes in pf to 0
+
+};
+
+var cherry = (g) => {
+
+    const pf = maxPath(g); // path forward
+    const pfv = pf.reduce((a,pair) => a + pathValue(g,pair), 0);
+    
+    const h = removeCherries(g, pf);
+    const pb = maxPath(h, false); // path forward
+    const pbv = pb.reduce((a,pair) => a + pathValue(h, pair), 0);
+
+    return pfv + pbv;
+};
+
+
+
+//
+// tests
+
+const inputExpectedPairs = [
+    [[ [] ], 0],
+    [[ [[1,1,-1],[1,-1,1],[-1,1,1]] ], 0],
+    [[ [[0,1,-1],[1,0,-1],[1,1,1]] ], 5]
+];
+
+module.exports = {
+    f: cherry,
+    inputExpectedPairs,
+    name: '741 [hard] - cherry pickup'
+};
