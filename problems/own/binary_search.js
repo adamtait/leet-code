082d597e73e6353e25997cbb4b87bfa9ad43dcd6
adamtait <bin@adamta.it>
2021-09-14 22:48:17 -0700

    problems/own/binary_search: better iterative implementation

--- problems/own/binary_search.js
+++ problems/own/binary_search.js
@@ -29,6 +29,22 @@ var bsi = (v, vs) => {
     return null;
 };
 
+var bsi = (v, vs) => {
+
+    var l = 0;
+    var r = vs.length - 1;
+    while ( l <= r ) {
+        const m = Math.floor( (r + l) / 2 );
+        const vv = vs[m];
+        if ( vv < v )      l = m + 1;
+        else if ( vv > v ) r = m - 1;
+        else return vv;
+    }
+    
+    return null;
+};
+
+
 var bsr = (v, vs) => {
 
     const len = vs.length;
@@ -62,29 +78,40 @@ var bsr = (v, vs) => {
  * Bonus points if you can solve both iteratively and recursively.
  */
 
+// 2, [1,3,4,5]  => 0
+// 7, [1,3,4,5]  => 3
 
-//var bsi = (v, vs) => {
-//
-//};
+var ubsi = (v, vs) => {
 
-var bsr = (v, vs) => {
-
-    const len = vs.length;
-    if ( len === 0 ) return null;
-    if ( len === 1 ) {
-        if ( vs[0] <= v )
-            return vs[0];
-        return null;
+    var l = 0;
+    var r = vs.length - 1;
+    while ( l <= r ) {
+        const m = Math.floor( (r + l) / 2 );
+        const vv = vs[m];
+        if ( vv < v )      l = m + 1;
+        else if ( vv > v ) r = m - 1;
+        else return m;
     }
+    return Math.min(l,r);
+};
+
+var ubsr = (v, vs) => {
+
+    console.log(vs);
+    const len = vs.length;
+    if ( len <= 1 ) return 0;
+    if ( len === 2 ) return 1;
     
     const i = Math.floor( len / 2 );
     const vv = vs[i];
 
-    if ( vv <= v && vs[i+1] >= v ) return vv;
-    if ( v < vv ) return bsr(v, vs.slice(0,i))
-
-    // else v > vv
-    return bsr(v, vs.slice(i+1));
+    if ( v === vv ) return i;
+    
+    if ( vv > v ) // => left
+        return ubsr(v, vs.slice(0,i+1))
+    
+    // vv < v => right
+    return i + ubsr(v, vs.slice(i))
 };
 
 
